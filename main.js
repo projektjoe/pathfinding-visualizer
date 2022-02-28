420aaa86fdafe0b1309696d878b97c2ab0bf6aaa
YoussefEmad99 <youssefemac@hotmail.com>
2022-02-28 17:04:43 +0200

    fixed some bugs

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+const NOTHING = 0
+const WALL = 1
+const START = 2
+const END = 3
 
+const START_ICON = 'url(assets/start.jpg)';
+const END_ICON = 'url(assets/end.jpg)';
 
 class Board{
     constructor(row,column){
@@ -6,9 +12,10 @@ class Board{
         this.NumOfSquaresColumn = column
         this.algorithm = "A*"
         this.numericalBoard = Array(this.numOfSquaresRow).fill().map(()=>Array(this.NumOfSquaresColumn).fill(0));
-        this.mouseClicked = false
-        this.startInHands = false
-        this.endInHands = false
+        this.mouseClicked = [false]
+        this.startInHands = [false]
+        this.endInHands = [false]
+        
     }
     
     initialize(){
@@ -19,6 +26,9 @@ class Board{
     }
     #createGameBoard(){
         let gameBoard = document.getElementById("board")
+        gameBoard.style["grid-template-columns"] = "repeat("+ this.NumOfSquaresColumn +", 1fr)";
+
+        // create squares and append them to the board
         for(let i=0; i<this.numOfSquaresRow; i++){
             for(let j = 0; j < this.NumOfSquaresColumn; j++){
             let square = document.createElement("div")
@@ -28,46 +38,55 @@ class Board{
         }
     }
     #initializeSquare(square,i,j){
-        square.classList.add("square")
-        square.setAttribute("row", i)
-        square.setAttribute("column",j)
-        square.style["user-select"] = "none"
-        square.style["-moz-user-select"] = "none"
-        square.style["-ms-user-select"] = "none"
-        square.style["-webkit-user-select"]= "none"
-        square.style["-webkit-user-drag"]= "none"
-        square.style["user-drag"] = "none"
-        square.style.backgroundSize = 'cover';
-        square.style.animationDuration = '1s';
-        square.style.animationName = 'gray_to_black';
+      //set the class and row/ column of the square
+      square.classList.add("square")
+      square.setAttribute("row", i)
+      square.setAttribute("column",j)
+
+      // to prevent dragging of the square
+      square.style["user-select"] = "none"
+      square.style["-moz-user-select"] = "none"
+      square.style["-ms-user-select"] = "none"
+      square.style["-webkit-user-select"]= "none"
+      square.style["-webkit-user-drag"]= "none"
+      square.style["user-drag"] = "none"
+
+      // for the pictures (start and end)
+      square.style.backgroundSize = 'cover';
+
+      // animations
+      square.style.animationDuration = '1s';
+      square.style.animationName = 'gray_to_black';
     }
-    #indices_to_square = function( i,  j){
+
+    #coordinatesToSquare = function(i,  j){
+      // takes in the row and the column and returns the desired square
         let query = "[row='" + i + "'][column='" + j + "']";
-        return query;
+        let square = document.querySelector(query)
+        return square;
     }
 
     #resetStartNodePosition(){
         this.numericalBoard[0][0] = 2
-        let query = this.#indices_to_square(0,0)
-        let square = document.querySelector(query)
-        square.style.backgroundImage = 'url(assets/start.jpg)';
+        this.#coordinatesToSquare(0,0).style.backgroundImage = START_ICON;
     }
+
     #resetEndNodePosition(){
-        this.numericalBoard[0][1] = 3
-        let query = this.#indices_to_square(0,1)
-        let square = document.querySelector(query)
-        square.style.backgroundImage = 'url(assets/end.jpg)';
+        this.numericalBoard[this.numOfSquaresRow-1][this.NumOfSquaresColumn-1] = 3
+        this.#coordinatesToSquare(this.numOfSquaresRow-1,this.NumOfSquaresColumn-1).style.backgroundImage = END_ICON;
     }
+
+    // make sure start and end node did not disappear, if so reset their positions
     #check_if_board_valid(){
         let startfound = false;
         let endfound = false;
       
-        for (let i=0; i<this.no_of_squares_row; i++){
-          for(let j =0; j < this.no_of_squares_column; j++){
-            if(board[i][j]==2){
+        for (let i=0; i<this.numOfSquaresRow; i++){
+          for(let j =0; j < this.NumOfSquaresColumn; j++){
+            if(this.numericalBoard[i][j]==2){
               startfound=true;
             }
-            if(board[i][j]==3){
+            if(this.numericalBoard[i][j]==3){
               endfound=true;
             }
           }
@@ -79,13 +98,14 @@ class Board{
             this.#resetEndNodePosition()
         }
       }
+
     mouseDownHandle(){
-        this.mouseClicked = true
+        this.mouseClicked[0] = true
     }
     mouseUpHandle(){
-        this.mouseClicked = false
-        this.startInHands = false
-        this.endInHands = false
+        this.mouseClicked[0] = false
+        this.startInHands[0] = false
+        this.endInHands[0] = false
         this.#check_if_board_valid()
     }
 
@@ -95,49 +115,49 @@ class Board{
             let r = this.attributes.row.nodeValue
             let c = this.attributes.column.nodeValue
     
-            if(Board[r][c] ===0){
+            if(Board[r][c] === NOTHING){
               this.style.backgroundColor = "cyan";
               Board[r][c] = 1;
               }
-              else if(Board[r][c]===1) {
+              else if(Board[r][c]=== WALL) {
                 this.style.backgroundColor = "black";
                 Board[r][c] = 0;
               }
-    
-            
           }
     }
     #addOnMouseOver(square){
+        
         let Board = this.numericalBoard
         let startInHands = this.startInHands
         let endInHands = this.endInHands
         let resetStartNodePosition = this.#resetStartNodePosition()
         let resetEndNodePosition = this.#resetEndNodePosition()
         square.onmouseover= function(){
+            debugger;
             let r = this.attributes.row.nodeValue
             let c = this.attributes.column.nodeValue
             this.style.backgroundColor = "white";
         
-            if (startInHands){
-              if(Board[r][c]!==3){
-              this.style.backgroundImage = 'url(assets/start.jpg)';
+            if (startInHands[0]){
+              if(Board[r][c]!== END){
+              this.style.backgroundImage = START_ICON;
               Board[r][c]=2;
               }
               // if user puts start in end block, return end block to default pos
-              else if(Board[r][c]===3){
-                this.style.backgroundImage = 'url(assets/start.jpg)';
+              else if(Board[r][c]===END){
+                this.style.backgroundImage = START_ICON;
                 Board[r][c]=2;
                 resetEndNodePosition();
               }
             }
     
-            if (endInHands){
-              if(Board[r][c]!==2){
-              this.style.backgroundImage = 'url(assets/end.jpg)';
+            if (endInHands[0]){
+              if(Board[r][c]!== START){
+              this.style.backgroundImage = END_ICON;
               Board[r][c]=3;
               }
-              else if (Board[r][c]===2){
-                this.style.backgroundImage = 'url(assets/end.jpg)';
+              else if (Board[r][c]=== START){
+                this.style.backgroundImage = END_ICON;
                 Board[r][c]=3;
                 resetStartNodePosition();
                 
@@ -155,64 +175,58 @@ class Board{
         square.onmouseout= function(){
             let r = this.attributes.row.nodeValue
             let c = this.attributes.column.nodeValue
-            debugger;
-            if(startInHands){
-                
-            }
-            if(endInHands){
-
-            }
-            if(!mouseClicked) {
+            let thisSquare = Board[r][c]
+            if(!mouseClicked[0]) {
               
-              if(Board[r][c] === 0){
+              if(thisSquare === NOTHING){
               this.style.backgroundColor = "black";
               }
-              else if (Board[r][c] ===1){
+              else if (thisSquare === WALL){
                 this.style.backgroundColor = "cyan";
                 this.style.animationName = "black_to_cyan"
               }
-              else if (Board[r][c] ===2){
-                this.style.backgroundImage = 'url(assets/start.jpg)';
+              else if (thisSquare === START){
+                this.style.backgroundImage = START_ICON;
               }
-              else if (Board[r][c] ===3){
-                this.style.backgroundImage = 'url(assets/end.jpg)';
+              else if (thisSquare === END){
+                this.style.backgroundImage = END_ICON;
               }
             }
             else{
-              if(Board[r][c] ===0){
+              if(thisSquare === NOTHING){
               this.style.backgroundColor = "cyan";
               this.style.animationName = "black_to_cyan"
               Board[r][c] = 1;
               }
-              else if(Board[r][c]===1) {
+              else if(thisSquare=== WALL) {
                 this.style.backgroundColor = "black";
                 Board[r][c] = 0;
               }
-              else if(Board[r][c]===2){
+              else if(thisSquare=== START){
                 
                 this.style.backgroundImage = "none";
                 this.style.backgroundColor = "black";
-                startInHands = true;
+                startInHands[0] = true;
                 Board[r][c]=0;
               }
-              else if(Board[r][c]===3){
+              else if(thisSquare=== END){
                 
                 this.style.backgroundImage = "none";
                 this.style.backgroundColor = "black";
-                endInHands = true;
+                endInHands[0] = true;
                 Board[r][c]=0;
               }
               
     
             }
+
           }
         
     }
     #addEventListenersToSquares(){
         for(let i=0; i<this.numOfSquaresRow; i++){
             for(let j = 0; j<this.NumOfSquaresColumn; j++){
-                let query = this.#indices_to_square(i.toString(),j.toString())
-                let square = document.querySelector(query)
+                let square = this.#coordinatesToSquare(i.toString(),j.toString())
                 
                 this.#addOnClick(square)
                 this.#addOnMouseOver(square)
@@ -223,11 +237,18 @@ class Board{
 
 }
 
-
+function algopick() {  
+    var mylist = document.getElementById("algolist");  
+    algo = mylist.options[mylist.selectedIndex].text;  
+}  
+  
+function start(){
+console.log(algo)
+}
 
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    board = new Board(26,25)
+    board = new Board(27,54)
     board.initialize()
 
 })
